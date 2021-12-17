f777b7eaf1455bf9a144a3a325d8cfce2c907a4b
Mark Holden <mark.holden@monscierge.com>
2021-12-17 01:24:32 -0600

    Add Classes for Encrypting and Decrypting

---
+++ cryptography/Decrypt.cs
@@ -0,0 +1,34 @@
+﻿namespace Cryptography
+{
+    public class Decrypt
+    {
+        const int D = 7;
+        const int N = 33;
+
+        public static void Run()
+        {
+            Console.WriteLine("Enter a phrase to decrypt:");
+            var input = Console.ReadLine();
+            if (input != null)
+            {
+                var plainText = new List<int>();
+                Console.WriteLine("Unencrypted:");
+                foreach (var value in input.Split(' '))
+                {
+                    var digit = int.Parse(value);
+                    var unencrypted = (int)(Math.Pow(digit, D) % N);
+                    plainText.Add(unencrypted);
+                    Console.Write(string.Format("{0:00}", unencrypted) + " ");
+                }                
+                
+                var message = "";
+                foreach (var value in plainText)
+                {
+                    message += Conversion.GetChar(value);
+                }
+
+                Console.WriteLine($"\nMessage:\n{message}");
+            }
+        }
+    }
+}
---
+++ cryptography/Encrypt.cs
@@ -0,0 +1,36 @@
+﻿namespace Cryptography
+{
+    public static class Encrypt
+    {
+        const int E = 3;
+        const int N = 33;
+
+        public static void Run()
+        {
+            Console.WriteLine("Enter a phrase to encrypt:");
+            var input = Console.ReadLine();
+            if (input != null)
+            {
+                var plainText = new List<int>();
+                foreach (var character in input.ToLower())
+                {
+                    plainText.Add(Conversion.GetInt(character));
+                }
+
+                Console.WriteLine("Unencrypted:");
+                var cypherText = new List<int>();
+                foreach (var value in plainText)
+                {
+                    Console.Write(string.Format("{0:00}", value) + " ");
+                    cypherText.Add((int)Math.Pow(value, E) % N);
+                }
+
+                Console.WriteLine("\nEncrypted:");
+                foreach (var value in cypherText)
+                {
+                    Console.Write(string.Format("{0:00}", value) + " ");
+                }
+            }
+        }
+    }
+}
