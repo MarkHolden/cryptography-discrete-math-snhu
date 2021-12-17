0173dad222a2d164c172f7ba504e50137c6f4cc8
Mark Holden <mark.holden@monscierge.com>
2021-12-17 01:25:05 -0600

    Add switch for Encrypt/Decrypt and update Conversion Dictionary

--- cryptography/Program.cs
+++ cryptography/Program.cs
@@ -1,67 +1,69 @@
-﻿const int E = 3;
-const int N = 33;
-
-Console.WriteLine("Enter a phrase to encrypt:");
-var input = Console.ReadLine();
-if (input != null)
+﻿namespace Cryptography
 {
-    var plainText = new List<int>();
-    foreach(var character in input.ToLower())
+    public class Program
     {
-        plainText.Add(Conversion.Table[character]);
+        public static void Main()
+        {
+            Console.WriteLine("Press E to Encrypt, and D to Decrypt.");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "E":
+                    Encrypt.Run();
+                    break;
+                case "D":
+                    Decrypt.Run();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry.");
+                    break;
+            }
+        }
     }
-
-    Console.WriteLine("Unencrypted:");
-    var cypherText = new List<int>();
-    foreach (var value in plainText)
+    public class Conversion
     {
-        Console.Write(string.Format("{0:00}", value) + " ");
-        cypherText.Add((int)Math.Pow(value, E) % N);
+        public static int GetInt(char lookup)
+        {
+            return table[lookup];
+        }
+        public static char GetChar(int lookup)
+        {
+            return table.FirstOrDefault(t => t.Value == lookup).Key;
+        }
+
+        private static Dictionary<char, int> table = new Dictionary<char, int>
+        {
+            { 'a', 1 },
+            { 'b', 2 },
+            { 'c', 3 },
+            { 'd', 4 },
+            { 'e', 5 },
+            { 'f', 6 },
+            { 'g', 7 },
+            { 'h', 8 },
+            { 'i', 9 },
+            { 'j', 10 },
+            { 'k', 11 },
+            { 'l', 12 },
+            { 'm', 13 },
+            { 'n', 14 },
+            { 'o', 15 },
+            { 'p', 16 },
+            { 'q', 17 },
+            { 'r', 18 },
+            { 's', 19 },
+            { 't', 20 },
+            { 'u', 21 },
+            { 'v', 22 },
+            { 'w', 23 },
+            { 'x', 24 },
+            { 'y', 25 },
+            { 'z', 26 },
+            { '.', 27 },
+            { ',', 28 },
+            { '!', 29 },
+            { '\'', 30 },
+            { ' ', 32 }
+        };
     }
-
-    Console.WriteLine("\nEncrypted:");
-    foreach (var value in cypherText)
-    {
-        Console.Write(string.Format("{0:00}", value) + " ");
-    }
-}
-
-public class Conversion
-{
-    public static Dictionary<char, int> Table = new Dictionary<char, int>
-    {
-        { 'a', 1 }, 
-        { 'b', 2 },
-        { 'c', 3 },
-        { 'd', 4 },
-        { 'e', 5 },
-        { 'f', 6 },
-        { 'g', 7 },
-        { 'h', 8 },
-        { 'i', 9 },
-        { 'j', 10 },
-        { 'k', 11 },
-        { 'l', 12 },
-        { 'm', 13 },
-        { 'n', 14 },
-        { 'o', 15 },
-        { 'p', 16 },
-        { 'q', 17 },
-        { 'r', 18 },
-        { 's', 19 },
-        { 't', 20 },
-        { 'u', 21 },
-        { 'v', 22 },
-        { 'w', 23 },
-        { 'x', 24 },
-        { 'y', 25 },
-        { 'z', 26 },
-        { ' ', 32 },
-        { '.', 33 },
-        { ',', 34 },
-        { '!', 35 },
-        { '?', 36 },
-        { '(', 37 },
-        { ')', 38 }
-    };
 }
